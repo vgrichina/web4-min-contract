12d846a08ecd645b644ca5ca8c08b25461551d02
Vladimir Grichina <vgrichina@gmail.com>
2022-11-28 22:23:23 -0800

    Some refactoring and comments

--- web4-min.zig
+++ web4-min.zig
@@ -1,14 +1,42 @@
 const std = @import("std");
 
+// NOTE: In smart contract context don't really have to free memory before execution ends
 var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
 var allocator = arena.allocator();
 
+// Import host functions provided by NEAR runtime.
+// See https://github.com/near/near-sdk-rs/blob/78c16447486285fd952765ef3e727e16d6c8c867/near-sdk/src/environment/env.rs#L117
 extern fn input(register_id: u64) void;
 extern fn read_register(register_id: u64, ptr: u64) void;
 extern fn register_len(register_id: u64) u64;
 extern fn value_return(value_len: u64, value_ptr: u64) void;
 extern fn log_utf8(len: u64, ptr: u64) void;
 
+// Helper wrapper functions for interacting with the host
+fn log(str: []const u8) void {
+    log_utf8(str.len, @ptrToInt(str.ptr));
+}
+
+fn valueReturn(value: []const u8) void {
+    value_return(value.len, @ptrToInt(value.ptr));
+}
+
+fn readRegisterAlloc(register_id: u64) []u8 {
+    const len = @truncate(usize, register_len(register_id));
+    // TODO: Explicit NEAR-compatible panic instead of unreachable?
+    const bytes = allocator.alloc(u8, len) catch unreachable;
+    read_register(register_id, @ptrToInt(bytes.ptr));
+    return bytes;
+}
+
+fn base64EncodeAlloc(data: []const u8) []const u8 {
+    const encoder = std.base64.standard.Encoder;
+    const dataSize = encoder.calcSize(data.len);
+    const dataBuffer = allocator.alloc(u8, dataSize) catch unreachable;
+    return encoder.encode(dataBuffer, data);
+}
+
+// Main entry point for web4 contract.
 export fn web4_get() void {
     const Web4Response = struct {
         contentType: []const u8,
@@ -19,13 +47,13 @@ export fn web4_get() void {
         // cacheControl: ?[]const u8,
     };
 
+    // Store method arguments blob in a register 0
     input(0);
-    const inputLength = @truncate(usize, register_len(0));
-    const inputData = allocator.alloc(u8, inputLength) catch unreachable;
-    read_register(0, @ptrToInt(inputData.ptr));
 
-    log_utf8(inputData.len, @ptrToInt(inputData.ptr));
+    // Read method arguments blob from register 0
+    const inputData = readRegisterAlloc(0);
 
+    // Parse method arguments JSON
     // NOTE: Parsing using TokenStream results in smaller binary than deserializing into object
     var requestStream = std.json.TokenStream.init(inputData);
     var lastString: [] const u8 = "";
@@ -42,14 +70,17 @@ export fn web4_get() void {
         }
     } else "/";
 
-    const body = std.fmt.allocPrint(allocator, "Hello from <b>{s}</b>!", .{path}) catch unreachable;
-    const encoder = std.base64.standard.Encoder;
-    const bodySize = encoder.calcSize(body.len);
-    const bodyBuffer = allocator.alloc(u8, bodySize) catch unreachable;
-    const base64Body = encoder.encode(bodyBuffer, body);
+    // Log request path
+    log(std.fmt.allocPrint(allocator, "path: {s}", .{path}) catch unreachable);
 
+    // Render response
+    const body = std.fmt.allocPrint(allocator, "Hello from <b>{s}</b>!", .{path}) catch unreachable;
+
+    // Construct response object
+    const base64Body = base64EncodeAlloc(body);
     const response = Web4Response{ .contentType = "text/html", .body = base64Body };
     const responseData = std.json.stringifyAlloc(allocator, response, .{}) catch unreachable;
 
-    value_return(responseData.len, @ptrToInt(responseData.ptr));
+    // Return method result
+    valueReturn(responseData);
 }
